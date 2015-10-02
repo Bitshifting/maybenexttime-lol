62a5053e1dcb2706b1b0bc276f734deb146794b4
Alex Kersten <alex@kersten.email>
2015-10-02 00:16:54 -0400

    add configuration

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'MNT'
+SITENAME = 'Maybe Next Time'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['static']
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (
+#    ('title', 'http://url.lol'),
+#    )
+
+# Social widget
+#SOCIAL = (
+#          ('title', 'https://url.lol'),
+#         )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+# Theme variables
+THEME='../pelican-themes/pelican-svbhack/'
+USER_LOGO_URL='http://maybenexttime.lol/static/logo.png'
+
